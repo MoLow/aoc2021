b210ac3ee0994ae1424f2d6a7665da899e93dc74
Moshe Atlow <moshea@testim.io>
2021-12-06 07:30:06 +0200

    fixim smixsim

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,4 +1,4 @@
-use std::cmp::{max, min};
+use std::cmp::{max};
 
 #[derive(Debug)]
 struct Line {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, env};
 
 mod day1;
 mod day2;
@@ -9,21 +9,32 @@ mod day5;
 fn get_input(day: u8) -> String {
     let mut file = String::from(""); //env::current_dir().unwrap().to_str().unwrap().to_owned();
     file.push_str(format!("src/day{}/input.txt", day).as_str());
-    let contents = fs::read_to_string(file).expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(file);
+    if contents.is_err() {
+        panic!("Could not read file, input.txt does not exist for day {}", day);
+    }
 
-    return contents;
+    return contents.expect("Could not read file, input.txt does not exist for day {}");
 }
 
 fn main() {
-    println!("################ Day 1 ################");
-    day1::run(get_input(1));
-    println!("################ Day 2 ################");
-    day2::run(get_input(2));
-    println!("################ Day 3 ################");
-    day3::run(get_input(3));
-    println!("################ Day 4 ################");
-    day4::run(get_input(4));
-    println!("################ Day 5 ################");
-    day5::run(get_input(5));
+    let args: Vec<String> = env::args().collect();
+    let day = args.get(1);
+    if day.is_none() {
+        println!("Please enter a valid day");
+        return;
+    }
 
+    let day = day.unwrap().parse().unwrap();
+    let input = get_input(day);
+
+    println!("################ Day {} ################", day);
+    match day {
+        1 => day1::run(input),
+        2 => day2::run(input),
+        3 => day3::run(input),
+        4 => day4::run(input),
+        5 => day5::run(input),
+        _ => println!("Please enter a valid da"),
+    }
 }
